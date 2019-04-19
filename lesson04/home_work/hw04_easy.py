5dbcbcfd15c17c7308d0ca3a6c031a6bae366a10
Alexander Petrovskiy <uiautomation@hotmail.com>
2019-04-19 20:51:17 +0300

    next changes

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -16,8 +16,14 @@ def squadratize(input_list):
     return [el ** 2 for el in input_list]
 
 
-print(expected_list == squadratize(source_list))
-print(expected_list.append(2) != squadratize(source_list))
+print(expected_list == squadratize(source_list[:]))
+source_list.append(2)
+expected_list_01 = expected_list[:]
+expected_list_01.append(2);
+print(expected_list_01 != squadratize(source_list[:]))
+expected_list_02 = expected_list[:]
+expected_list_02.append(4)
+print(expected_list_02 == squadratize(source_list[:]))
 print('===============================================================')
 
 # Задание-2:
@@ -41,12 +47,12 @@ print('===============================================================')
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
-source_list = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+source_list = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 
 def get_number_by_rules(list01):
     return [el for el in list01 if 0 == el % 3 and el > 0 and 0 != el % 4]
 
 
-print(get_number_by_rules(source_list))
+print([3, 6, 9] == get_number_by_rules(source_list))
 print('===============================================================')
