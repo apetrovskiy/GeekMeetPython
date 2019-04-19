5c2f315d4597b1f46802a3e3c655ec49008761d2
Alexander-Petrovskiy <alexander.petrovskiy@aurea.com>
2019-04-19 08:05:15 +0300

    the initial commit

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -8,9 +8,32 @@ __author__ = 'Петровский А.Е.'
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+source_list = [1, 2, 4, 0]
+expected_list = [1, 4, 16, 0]
+
+
+def squadratize(input_list):
+    return [el ** 2 for el in input_list]
+
+
+print(expected_list == squadratize(source_list))
+print(expected_list.append(2) != squadratize(source_list))
+print('===============================================================')
+
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+list_001 = ['яблоко', 'груша', 'айва', 'манго']
+list_002 = ['яблоко', 'папайя', 'манго']
+
+
+def get_common_items(list01, list02):
+    return [el for el in list01 if list02.__contains__(el)]
+
+
+print(get_common_items(list_001, list_002))
+print('===============================================================')
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -18,3 +41,12 @@ __author__ = 'Петровский А.Е.'
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+source_list = [-7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def get_number_by_rules(list01):
+    return [el for el in list01 if 0 == el % 3 and el > 0 and 0 != el % 4]
+
+
+print(get_number_by_rules(source_list))
+print('===============================================================')
--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -23,6 +23,8 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
 
+print('===============================================================')
+
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
@@ -47,9 +49,14 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+print('===============================================================')
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+
+print('===============================================================')
