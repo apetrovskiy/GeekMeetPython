4551dfa02e14bfec0cc8759cb63ad4994436f089
Alexander Petrovskiy <uiautomation@hotmail.com>
2019-04-24 21:58:45 +0300

    started hard task

--- lesson05/home_work/hw05_hard.py
+++ lesson05/home_work/hw05_hard.py
@@ -17,4 +17,69 @@ __author__ = 'Петровский А.Е.'
 # P.S. По возможности, сделайте кросс-платформенную реализацию.
 
 
+
+
+# Данный скрипт можно запускать с параметрами:
+# python with_args.py param1 param2 param3
+import os
+import sys
+print('sys.argv = ', sys.argv)
+
+
+def print_help():
+    print("help - получение справки")
+    print("mkdir <dir_name> - создание директории")
+    print("ping - тестовый ключ")
+    print("cp <file_name> - создает копию указанного файла")
+    print("rm <file_name> - удаляет указанный файл (запросить подтверждение операции)")
+    print("cd <full_path or relative_path> - меняет текущую директорию на указанную")
+    print("ls - отображение полного пути текущей директории")
+
+
+def make_dir():
+    if not dir_name:
+        print("Необходимо указать имя директории вторым параметром")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.mkdir(dir_path)
+        print('директория {} создана'.format(dir_name))
+    except FileExistsError:
+        print('директория {} уже существует'.format(dir_name))
+
+
+def ping():
+    print("pong")
+
+def cp():
+    return ""
+
+def rm():
+    return ""
+
+do = {
+    "help": print_help,
+    "mkdir": make_dir,
+    "ping": ping
+}
+
+try:
+    dir_name = sys.argv[2]
+except IndexError:
+    dir_name = None
+
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+
+
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        print("Задан неверный ключ")
+        print("Укажите ключ help для получения справки")
+
+
 print('==================================================================')
