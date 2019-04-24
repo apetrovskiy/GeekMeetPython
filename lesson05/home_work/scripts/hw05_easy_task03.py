96e17133816a4de6d329118286509f2d494f43d7
Alexander Petrovskiy <uiautomation@hotmail.com>
2019-04-24 16:39:11 +0300

    easy task 03

--- lesson05/home_work/scripts/hw05_easy_task03.py
+++ lesson05/home_work/scripts/hw05_easy_task03.py
@@ -1,2 +1,16 @@
 __author__ = 'Петровский А.Е.'
 
+import os
+
+root_path = os.getcwd();
+print(__file__)
+result_file_path = __file__ + "_copy.py"
+print(result_file_path)
+
+with open(__file__, 'r', encoding='UTF-8') as f:
+    file_content = f.readlines()
+    with open(result_file_path, 'w', encoding='UTF-8') as f2:
+        f2.write("".join(file_content))
+
+with open(result_file_path, 'r', encoding='UTF-8') as f3:
+    print(f3.readlines())
