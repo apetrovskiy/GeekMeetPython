5d59175138f9aa414cddc4142aab5733ccdb7a32
Alexander Petrovskiy <uiautomation@hotmail.com>
2019-04-24 17:52:18 +0300

    normal task in progress

--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -16,5 +16,65 @@ __author__ = 'Петровский А.Е.'
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
 
+import os
+import sys
+import lesson05.home_work.hw05_easy as easy
+
+
+# easy.task01script01
+# easy.task01script02
+# easy.task02
+# easy.task03
+
+def help():
+    print("help  - getting help")
+    print("cd    - go to a folder")
+    print("ls    - list files and subfolders")
+    print("rmdir - remove a folder")
+    print("mkdir - create a folder")
+
+
+def ls(path_to_dir):
+    try:
+        print(os.listdir(path_to_dir))
+    except FileNotFoundError:
+        print("Directory {} does not exist or inaccessible".format(path_to_dir))
+
+
+def rmdir():
+    try:
+        easy.task01script02.remove_dirs(dir_name, 1)
+    except:
+        print("Directory {} does not exist or inaccessible".format(path_to_dir))
+
+
+def mkdir():
+    try:
+        easy.task01script01.create_dirs(dir_name, 1)
+    except:
+        print("Directory {} does not exist or inaccessible".format(path_to_dir))
+
+
+do = {
+    "help": help,
+    # "cd": cd,
+    "ls": ls,
+    "rmdir": rmdir,
+    "mkdir": mkdir
+}
+
+try:
+    dir_name = sys.argv[2]
+except IndexError:
+    dir_name = None
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        help()
 
 print('==================================================================')
